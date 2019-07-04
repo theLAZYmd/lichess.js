307d9e9eae5826bbcd9b260e2ad659b40b964a97
theLAZYmd <lazymeep+github@gmail.com>
2019-07-04 02:44:20 +0100

    Fixed bug in StatusUser triggering safety compiler

--- src/structures/StatusUser.js
+++ src/structures/StatusUser.js
@@ -1,5 +1,5 @@
 const config = require('../config.js');
-const Base = require('./base');
+const Base = require('./Base');
 
 /**
  * Represents a Lichess user in a tournament
@@ -18,82 +18,82 @@ const Base = require('./base');
  */
 class StatusUser extends Base {
 
-    constructor(data) {
-        super();
+	constructor(data) {
+		super();
 
-        /**
+		/**
          * ID of a user taken from their username
          */
-        this.id = data.id;
+		this.id = data.id;
 
-        /**
+		/**
          * ID of a user taken from the 'name' property
          */
-        this.username = data.name;
+		this.username = data.name;
 
-        /**
+		/**
          * A user's title on Lichess
          * @type {string}
          * @name StatusUser#title
          * @readonly
          */
-        if (data.title && data.title !== "BOT") this.title = data.title;
+		if (data.title && data.title !== 'BOT') this.title = data.title;
 
                 
-        /**
+		/**
          * Whether or not the user is online
          * @type {Boolean}
          * @name StatusUser#online
          * @readonly
          */
-        this.online = Boolean(data.online);
+		this.online = Boolean(data.online);
                 
-        /**
+		/**
          * Whether or not the user is playing
          * @type {Boolean}
          * @name StatusUser#playing
          * @readonly
          */
-        this.playing = Boolean(data.playing);
+		this.playing = Boolean(data.playing);
                 
-        /**
+		/**
          * Whether or not the user is streaming
          * @type {Boolean}
          * @name StatusUser#streaming
          * @readonly
          */
-        this.streaming = Boolean(data.streaming);
+		this.streaming = Boolean(data.streaming);
                 
-        /**
+		/**
          * Whether or not the user is a patron
          * @type {Boolean}
          * @name StatusUser#patron
          * @readonly
          */
-        this.patron = Boolean(data.patron);
-    }
+		this.patron = Boolean(data.patron);
+	}
 
-    /**
+	/**
      * The URL to a user's game, if they are playing one
      * @type {string}
      * @name User#url
      * @readonly
      */
-    get gameURL () {
-        if (!this.playing) return undefined;
-        return `${config.uri}/@/${this.username}/tv`
-    }
+	get gameURL () {
+		if (!this.playing) return undefined;
+		return `${config.uri}/@/${this.username}/tv`;
+	}
     
-    /**
+	/**
      * The URL to a user's stream, if they are streaming
      * @type {string}
      * @name User#url
      * @readonly
      */
-    get streamURL () {
-        if (!this.streaming) return undefined;
-        return `${config.uri}/@/${this.username}/tv`
-    }
+	get streamURL () {
+		if (!this.streaming) return undefined;
+		return `${config.uri}/@/${this.username}/tv`;
+	}
 
 }
 
